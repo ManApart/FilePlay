69018d8449b62dce97d5009668c5c7ee8c369c2e
iceburg333 <ice_burg333@yahoo.com>
2018-06-16 13:05:42 -0400

    initial commit

---
+++ src/main/java/rak/echo/filePlay/FilePlayApplication.java
@@ -0,0 +1,12 @@
+package rak.echo.filePlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilePlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilePlayApplication.class, args);
+	}
+}
---
+++ src/main/java/rak/echo/filePlay/TestController.java
@@ -0,0 +1,15 @@
+package rak.echo.filePlay;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
